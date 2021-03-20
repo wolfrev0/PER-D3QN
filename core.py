5bea0cc26d6dbaf8fc84d3f65cace61772098273
0xC0DEF <lobo_prix@naver.com>
2021-03-20 17:08:46 +0900

    refactoring

--- core.py
+++ core.py
@@ -4,12 +4,3 @@ DEVICE = tc.device("cuda" if CUDA_AVAILABLE else "cpu")
 
 import matplotlib.pyplot as plt
 from IPython import display
-
-def render_inline(env):
-	plt.figure(3)
-	plt.clf()
-	plt.imshow(env.render(mode='rgb_array'))
-	plt.axis('off')
-
-	display.clear_output(wait=True)
-	display.display(plt.gcf())
